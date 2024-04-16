454dfabcce09a929394c17423b5235086ab4b186
Nive9 <29470670+Nive9@users.noreply.github.com>
2024-04-16 23:44:18 +0200

    Fetch browser's primary language

--- assets/js/lang.js
+++ assets/js/lang.js
@@ -3,13 +3,13 @@
   const response = await fetch(`languages/${lang}.json`);
   return response.json();
 }
-
+/* 
 // Function to set the language preference
 function setLanguagePreference(lang) {
   localStorage.setItem('language', lang);
   location.reload();
 }
-
+ */
 // Function to update content based on selected language
 function updateContent(langData) {
   document.querySelectorAll('[data-i18n]').forEach(element => {
@@ -30,7 +30,7 @@ async function changeLanguage(lang) {
 
 // Call updateContent() on page load
 window.addEventListener('DOMContentLoaded', async () => {
-  const userPreferredLanguage = localStorage.getItem('language') || 'en';
+  const userPreferredLanguage = navigator.language || 'en';
   const langData = await fetchLanguageData(userPreferredLanguage);
   updateContent(langData);
   // toggleArabicStylesheet(userPreferredLanguage);
